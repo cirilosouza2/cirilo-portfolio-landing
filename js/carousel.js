b3f18667e4a7b67dfb713b2389c157faf51b400e
Cirilo <cirilo.souza@gmail.com>
2026-07-29 16:50:35 -0300

    pagina apepe

--- js/carousel.js
+++ js/carousel.js
@@ -10,46 +10,76 @@ $(function () {
         pullDrag: true
     };
 
-    $('#case-grid').owlCarousel($.extend(true, {}, carouselBaseOptions, {
-        margin: 18,
-        autoplayTimeout: 4000,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            576: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            1024: {
-                items: 3
-            },
-            1440: {
-                items: 4
-            }
-        }
-    }));
+    var $caseGrid = $('#case-grid');
+    var $empresasCarousel = $('#empresas-carousel');
+    var $telasCarousel = $('#telas-carousel');
 
-    $('#empresas-carousel').owlCarousel($.extend(true, {}, carouselBaseOptions, {
-        margin: 30,
-        autoplayTimeout: 2500,
-        dots: false,
-        responsive: {
-            0: {
-                items: 2
-            },
-            768: {
-                items: 4
-            },
-            1024: {
-                items: 5
-            },
-            1400: {
-                items: 6
+    if ($caseGrid.length) {
+        $caseGrid.owlCarousel($.extend(true, {}, carouselBaseOptions, {
+            margin: 18,
+            autoplayTimeout: 4000,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                576: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                1024: {
+                    items: 3
+                },
+                1440: {
+                    items: 4
+                }
             }
-        }
-    }));
+        }));
+    }
+
+    if ($empresasCarousel.length) {
+        $empresasCarousel.owlCarousel($.extend(true, {}, carouselBaseOptions, {
+            margin: 30,
+            autoplayTimeout: 2500,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                768: {
+                    items: 4
+                },
+                1024: {
+                    items: 5
+                },
+                1400: {
+                    items: 6
+                }
+            }
+        }));
+    }
+
+    if ($telasCarousel.length) {
+        $telasCarousel.owlCarousel($.extend(true, {}, carouselBaseOptions, {
+            margin: 24,
+            autoplayTimeout: 2500,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                1024: {
+                    items: 4
+                },
+                1400: {
+                    items: 4
+                }
+            }
+        }));
+    }
 });
